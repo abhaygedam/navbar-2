4d042a22bef3677ace3fb02c1008d844afdcfc93
abhaygedam <abhaygedam1999@gmail.com>
2021-09-17 22:13:52 +0530

    slidebar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import NavApp from './Component/NavApp';
+import SlideBar from './Component/Slidebar';
 
 function App() {
   return (
     <div className="App">
-     <NavApp/>
+      {/* <NavApp /> */}
+      <SlideBar></SlideBar>
     </div>
   );
 }
